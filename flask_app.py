a12bc74e459d1ee6db5126b502fe13f4cc5f07b3
Leticia Garcia <lgarci51@uic.edu>
2021-09-12 23:17:59 -0500

    Update flask_app.py
    - Added prediction route
    - Transform the image
    - Predicted the label image

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,36 @@
-from flask import Flask
+#Importing libraries we need to run the model 
+from flask import Flask, request
+from PIL import Image 
+import numpy as np
+from skimage import transform 
+import tensorflow as tf
 
 #Created a flask application with the name (__name__)
 app = Flask(__name__)
+model = tf.keras.models.load_model('Models/my_save_model.h5')
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+#loads saved CNN model and had the class names on here to help with labeling the prediction
 
 #Creating a route for the website 
 @app.route("/")
 def home():
-    return "Prediction"
+    return "IDS 594 Project"
 
+#Post to use for sending something to the website 
+#Also had to upload image and transform it to match dimensions for the CNN
+@app.route("/pred/", methods=["POST"])
+def pred():
+    file = request.files['image']
+    image = Image.open(file.stream)
+    image = np.array(image).astype('float32')/255
+    image = transform.resize(image, (32, 32, 3))
+    image = np.expand_dims(image, axis=0)
+    #Predicting and getting class label 
+    predict = model.predict(image)
+    label = predict.argmax(axis=-1)
+    class_name = class_names[label[0]]
+    return class_name
 
 #launches when you run the program 
 if __name__ == '__main__':
