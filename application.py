40400a2810056ff3a9f118ede5cff9063b25d6ce
Leticia Garcia <lgarci51@uic.edu>
2021-09-18 20:04:38 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,21 +1,22 @@
 #Importing libraries we need to run the model 
 from flask import Flask, request
-# from PIL import Image 
-# import numpy as np
-# from skimage import transform 
-# import tensorflow as tf
-# import os
+from PIL import Image 
+import numpy as np
+from skimage import transform 
+import tensorflow as tf
+import os
 
 # abspath = os.path.abspath(__file__)
 # dname = os.path.dirname(abspath)
 # os.chdir(dname)
 
+model = tf.keras.models.load_model('Models/my_save_model.h5')
 #Created a flask application with the name (__name__)
 application = Flask(__name__)
 
-# class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-#                'dog', 'frog', 'horse', 'ship', 'truck']
-# #loads saved CNN model and had the class names on here to help with labeling the prediction
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+#loads saved CNN model and had the class names on here to help with labeling the prediction
 
 #Creating a route for the website 
 @application.route("/")
@@ -24,19 +25,18 @@ def home():
 
 #Post to use for sending something to the website 
 #Also had to upload image and transform it to match dimensions for the CNN
-# @application.route("/pred/", methods=["POST"])
-# def pred():
-#     model = tf.keras.models.load_model('Models/my_save_model.h5')
-#     file = request.files['image']
-#     image = Image.open(file.stream)
-#     image = np.array(image).astype('float32')/255
-#     image = transform.resize(image, (32, 32, 3))
-#     image = np.expand_dims(image, axis=0)
-#     #Predicting and getting class label 
-#     predict = model.predict(image)
-#     label = predict.argmax(axis=-1)
-#     class_name = class_names[label[0]]
-#     return class_name
+@application.route("/pred/", methods=["POST"])
+def pred():
+    file = request.files['image']
+    image = Image.open(file.stream)
+    image = np.array(image).astype('float32')/255
+    image = transform.resize(image, (32, 32, 3))
+    image = np.expand_dims(image, axis=0)
+    #Predicting and getting class label 
+    predict = model.predict(image)
+    label = predict.argmax(axis=-1)
+    class_name = class_names[label[0]]
+    return class_name
 
 #launches when you run the program 
 if __name__ == '__main__':
